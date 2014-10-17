d0a7b4032062752ab8dffa66fe1e987d81c1107b
James T. Sondag II <jamessondag@gmail.com>
2014-10-16 20:11:27 -0600

    no message

---
+++ simpleloop1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jamessondag on 10/16/14.
+ */
+function findLongestWord(str){
+    var longest = "";
+    var words = str.trim().split(" ");
+    for (var longestWord = 0; longestWord < word.length; i++){
+        var word = word[longestWord];
+        if (word.length > longest.length) {
+    }
+
+return longest
+}
+
+console.log(findLongestWord("hello my name is James Sondag")
